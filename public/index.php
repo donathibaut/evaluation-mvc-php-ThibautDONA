5cd2f3c9ff222a08681d25305ad97811c65216fb
ThibautDona <donathibaut02@gmail.com>
2026-05-10 14:46:19 +0200

    index to home redirection

--- public/index.php
+++ public/index.php
@@ -1,11 +1,6 @@
 <?php
 
 require_once __DIR__ . '/../vendor/autoload.php';
-
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../env');
-$dotenv->load();
-
-$readUser = new \App\Controllers\UserController();
-$readUser->readUser();
+include __DIR__ . '/../templates/home.php';
 
 ?>
---
+++ templates/home.php
@@ -0,0 +1,116 @@
+
+<!doctype html>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Accueil</title>
+  </head>
+  <body>
+    <header>
+      <h1 class="logo">Touche pas au klaxon</h1>
+      <nav>
+        <ul>
+          <li><button class="navBtn">Créer un trajet</button></li>
+          <li>Bonjour Xxxx Xxxx</li>
+          <li><button class="navBtn">Déconnexion</button></li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <table>
+        <caption>
+          <h2>Trajets proposés</h2>
+        </caption>
+        <thead>
+          <tr>
+            <th scope="col">Départ</th>
+            <th scope="col">Date</th>
+            <th scope="col">Heure</th>
+            <th scope="col">Destination</th>
+            <th scope="col">Date</th>
+            <th scope="col">Heure</th>
+            <th scope="col">Places</th>
+            <th scope="col">Interactions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="start">x</td>
+            <td class="date">xx/xx/xx</td>
+            <td class="hour">xx:xx</td>
+            <td class="destination">x</td>
+            <td class="date">xx/xx/xx</td>
+            <td class="hour">xx:xx</td>
+            <td class="nbSeats">x</td>
+            <td>
+              <button class="seeDrive">Voir</button>
+              <!-- modifyDrive & deleteDrive if it's your planned drive -->
+              <button class="modifyDrive">Modifier</button>
+              <button class="deleteDrive">Supprimer</button>
+            </td>
+          </tr>
+          <tr>
+            <td class="start">x</td>
+            <td class="date">xx/xx/xx</td>
+            <td class="hour">xx:xx</td>
+            <td class="destination">x</td>
+            <td class="date">xx/xx/xx</td>
+            <td class="hour">xx:xx</td>
+            <td class="nbSeats">x</td>
+            <td>
+              <button class="seeDrive">Voir</button>
+              <button class="modifyDrive">Modifier</button>
+              <button class="deleteDrive">Supprimer</button>
+            </td>
+          </tr>
+          <tr>
+            <td class="start">x</td>
+            <td class="date">xx/xx/xx</td>
+            <td class="hour">xx:xx</td>
+            <td class="destination">x</td>
+            <td class="date">xx/xx/xx</td>
+            <td class="hour">xx:xx</td>
+            <td class="nbSeats">x</td>
+            <td>
+              <button class="seeDrive">Voir</button>
+              <button class="modifyDrive">Modifier</button>
+              <button class="deleteDrive">Supprimer</button>
+            </td>
+          </tr>
+          <tr>
+            <td class="start">x</td>
+            <td class="date">xx/xx/xx</td>
+            <td class="hour">xx:xx</td>
+            <td class="destination">x</td>
+            <td class="date">xx/xx/xx</td>
+            <td class="hour">xx:xx</td>
+            <td class="nbSeats">x</td>
+            <td>
+              <button class="seeDrive">Voir</button>
+              <button class="modifyDrive">Modifier</button>
+              <button class="deleteDrive">Supprimer</button>
+            </td>
+          </tr>
+          <tr>
+            <td class="start">x</td>
+            <td class="date">xx/xx/xx</td>
+            <td class="hour">xx:xx</td>
+            <td class="destination">x</td>
+            <td class="date">xx/xx/xx</td>
+            <td class="hour">xx:xx</td>
+            <td class="nbSeats">x</td>
+            <td>
+              <button class="seeDrive">Voir</button>
+              <button class="modifyDrive">Modifier</button>
+              <button class="deleteDrive">Supprimer</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </main>
+    <footer>
+      <p class="copyright">© 2024 - CENEF - MVC PHP</p>
+    </footer>
+  </body>
+</html>
